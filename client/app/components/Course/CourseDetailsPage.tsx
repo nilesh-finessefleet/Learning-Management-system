767ee331953c4cf1bcdd8ec9146b1da880034947
nilesh-finessefleet <nilesh@engg.finessefleet.com>
2024-03-19 12:24:49 +0530

    Initial commit

---
+++ client/app/components/Course/CourseDetailsPage.tsx
@@ -0,0 +1,83 @@
+import { useGetCourseDetailsQuery } from "@/redux/features/courses/coursesApi";
+import React, { useEffect, useState } from "react";
+import Loader from "../Loader/Loader";
+import Heading from "@/app/utils/Heading";
+import Header from "../Header";
+import Footer from "../Footer";
+import CourseDetails from "./CourseDetails";
+import {
+  useCreatePaymentIntentMutation,
+  useGetStripePublishablekeyQuery,
+} from "@/redux/features/orders/ordersApi";
+import { loadStripe } from "@stripe/stripe-js";
+import { useLoadUserQuery } from "@/redux/features/api/apiSlice";
+
+type Props = {
+  id: string;
+};
+
+const CourseDetailsPage = ({ id }: Props) => {
+  const [route, setRoute] = useState("Login");
+  const [open, setOpen] = useState(false);
+  const { data, isLoading } = useGetCourseDetailsQuery(id);
+  const { data: config } = useGetStripePublishablekeyQuery({});
+  const [createPaymentIntent, { data: paymentIntentData }] =
+    useCreatePaymentIntentMutation();
+  const { data: userData } = useLoadUserQuery(undefined, {});
+  const [stripePromise, setStripePromise] = useState<any>(null);
+  const [clientSecret, setClientSecret] = useState("");
+
+  useEffect(() => {
+    if (config) {
+      const publishablekey = config?.publishablekey;
+      setStripePromise(loadStripe(publishablekey));
+    }
+    if (data && userData?.user) {
+      const amount = Math.round(data.course.price * 100);
+      createPaymentIntent(amount);
+    }
+  }, [config, data, userData]);
+
+  useEffect(() => {
+    if (paymentIntentData) {
+      setClientSecret(paymentIntentData?.client_secret);
+    }
+  }, [paymentIntentData]);
+
+  return (
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          <Heading
+            title={data?.course?.name + " - FinesseFleet"}
+            description={
+              "Discover the power of personalized guidance at FinesseFleet! We firmly believe in unlocking the unique potential within every student. Tailored to individual preferences, our guidance ensures students can excel confidently in their chosen fields. Join us on the journey to academic excellence!"
+            }
+            keywords={data?.course?.tags}
+          />
+          <Header
+            route={route}
+            setRoute={setRoute}
+            open={open}
+            setOpen={setOpen}
+            activeItem={1}
+          />
+          {stripePromise && (
+            <CourseDetails
+              data={data.course}
+              stripePromise={stripePromise}
+              clientSecret={clientSecret}
+              setRoute={setRoute}
+              setOpen={setOpen}
+            />
+          )}
+          <Footer />
+        </>
+      )}
+    </>
+  );
+};
+
+export default CourseDetailsPage;
---
+++ client/app/components/Loader/Loader.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Loader.css";
+
+const Loader = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <img className="loader"></img>
+    </div>
+  );
+};
+
+export default Loader;
