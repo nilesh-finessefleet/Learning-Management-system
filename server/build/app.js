2577a7be7489080125f29657d791370ced9a8a87
nilesh-finessefleet <nilesh@engg.finessefleet.com>
2024-04-04 17:26:42 +0530

    updated server/build/app.js file

--- server/build/app.js
+++ server/build/app.js
@@ -23,7 +23,7 @@ exports.app.use(express_1.default.json({ limit: "50mb" }));
 exports.app.use((0, cookie_parser_1.default)());
 // cors => cross origin resource sharing
 exports.app.use((0, cors_1.default)({
-    origin: ["http://localhost:3000", "http://example1.com"],
+    origin: "https://lms-silk-five.vercel.app",
     credentials: true
 }));
 // api requests limit
